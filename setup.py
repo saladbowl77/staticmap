2ee5edeb4078e48e0ed215ce3b5ba85667200374
saladbowl77 <59104818+saladbowl77@users.noreply.github.com>
2024-07-04 00:31:05 +0900

    fix : pythonのバージョンの制限を一旦解除

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name="staticmap2",
     packages=["staticmap2"],
-    version="0.3.1",
+    version="0.3.2",
     description="A small, python-based library for creating map images with lines and markers.",
     author="Christoph Lingg",
     author_email="christoph@komoot.de",
@@ -11,5 +11,5 @@ setup(
     download_url="https://github.com/komoot/staticmap/tarball/0.1",
     keywords="static map image osm",
     classifiers=[],
-    install_requires=["Pillow", "requests", 'futures;python_version<"3.2"'],
+    install_requires=["Pillow", "requests"],
 )
